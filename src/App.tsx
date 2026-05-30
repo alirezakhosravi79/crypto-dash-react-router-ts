3b748b74824c0deb0ea6e54c9a8371c9ba956339
alirezass <alirezakhosraviss1526@gmail.com>
2026-05-31 02:45:29 +0330

    limit selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,67 @@
 import { useEffect, useState } from "react";
+import CoinCard from "./components/CoinCard";
+import LimitSelector from "./components/LimitSelector";
 
-const API_URL =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+const API_URL: string = import.meta.env.VITE_API_URL;
+
+export interface Coin {
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  price_change_percentage_24h: number;
+  total_volume: number;
+  high_24h: number;
+  low_24h: number;
+}
 
 function App() {
-
-  const [coins , setCoins] = useState([]);
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
+  const [coins, setCoins] = useState<Coin[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const [limit, setLimit] = useState<number>(10);
 
   useEffect(() => {
-    fetch(API_URL)
-    .then((res) => {
-      if (!res.ok) throw new Error ('Failed to fetch data');
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data)
-      setCoins(data);
-      setLoading(false)
-    })
-    .catch((err) => {
-      setError(err.message)
-      setLoading(false)
-    })
-  },[])
+    const fetchCoins = async (): Promise<void> => {
+      try {
+        const res: Response = await fetch(
+          `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`,
+        );
+        if (!res.ok) throw new Error("Failed to fetch data");
+        const data: Coin[] = await res.json();
+        console.log(data);
+        setCoins(data);
+      } catch (err: unknown) {
+        setError(err instanceof Error ? err.message : "An error occurred");
+        // setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  return <div>🚀 Crypto Dash</div>;
+    fetchCoins();
+  }, [limit]);
 
-} 
+  return (
+    <div>
+      <h1>🚀 Crypto Dash</h1>
+      {loading && <p>Loading....</p>}
+      {error && <div className="error">{error}</div>}
+
+      <LimitSelector limit={limit} onLimitChange={setLimit} />
+      
+      {!loading && !error && (
+        <main className="grid">
+          {coins.map((coin) => (
+            <CoinCard coin={coin} key={coin.id} />
+          ))}
+        </main>
+      )}
+    </div>
+  );
+}
 
 export default App;
